5cb8991908af50e047e0139c1cf91d1548b2622c
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-02-07 16:41:03 +0100

    Add operate function

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
+let num1;
+let operator;
+let num2;
+
 function add(a, b) {
   return Number(a) + Number(b);
 }
@@ -12,4 +16,19 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      break;
+  }
 }
